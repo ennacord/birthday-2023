fc44326e3ab1fe415da7dc68563a71d42e1702a6
dragonjet <jetriconew@gmail.com>
2022-12-02 23:35:01 -0800

    Setup Phaser 3

--- src/main.js
+++ src/main.js
@@ -1,1 +1,23 @@
 import './style.css';
+
+import Phaser from 'phaser';
+// import scene from './scenes';
+// import plugins from './plugins';
+
+// Phaser Game Instance
+// eslint-disable-next-line no-new
+new Phaser.Game({
+  type: Phaser.AUTO,
+  parent: 'game',
+  banner: false,
+  disableContextMenu: true,
+  backgroundColor: Phaser.Display.Color.HexStringToColor('#d0d0d0').color,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: 1920,
+    height: 937,
+  },
+  // plugins,
+  // scene,
+});
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   root: './src',
   publicDir: '../public',
+  define: {
+    CANVAS_RENDERER: JSON.stringify(true),
+    WEBGL_RENDERER: JSON.stringify(true),
+  },
   build: {
     outDir: '../docs',
     assetsInlineLimit: 0,
