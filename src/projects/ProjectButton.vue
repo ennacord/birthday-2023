7f7616febd1f36db68251ada9e814ab0d1e6d185
dragonjet <jetriconew@gmail.com>
2023-02-19 00:19:06 -0800

    Websites display update, picrew

--- src/projects/ProjectButton.vue
+++ src/projects/ProjectButton.vue
@@ -1,49 +1,51 @@
 <template>
-  <v-container class="container pa-0 ma-0">
-    <v-row no-gutters>
-      <v-col cols="6">
-        <div class="pa-4 pr-2">
-          <h2 class="text-center">Enna Button</h2>
-          <h4 class="text-center">
-            A dedicated site for a soundboard / button to play a sepcific Enna voiceline
-          </h4>
-          <h4 class="text-center mb-2">
-            <a href="https://bishbuttonbuild20230212.github.io" class="text-decoration-none" target="_blank">
-              https://bishbuttonbuild20230212.github.io
-            </a>
-          </h4>
-          <a href="https://bishbuttonbuild20230212.github.io" target="_blank">
-            <v-img :src="buttonImgUrl" cover />
-          </a>
-        </div>
-      </v-col>
-      <v-col cols="6">
-        <div class="pa-4 pl-2">
-          <h2 class="text-center">picrew</h2>
-          <h4 class="text-center">
-            ?
-          </h4>
-          <h4 class="text-center mb-2">
-            <a href="/" class="text-decoration-none" target="_blank">
-              ?
-            </a>
-          </h4>
-          <a href="/" target="_blank">
-            <v-img :src="buttonImgUrl" cover />
-          </a>
-        </div>
-      </v-col>
-    </v-row>
-  </v-container>
+  <div class="sites">
+    <div class="site">
+      <v-card height="100%">
+        <a href="https://bishbuttonbuild20230212.github.io" target="_blank" class="text-decoration-none">
+          <v-img :src="buttonImgUrl" gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.7)" class="align-end" cover height="100%">
+            <v-card-title class="text-white pb-8">
+              <h2 class="text-center mb-2">Enna Bish Button</h2>
+              <p class="text-center site-desc">
+                A dedicated site for a soundboard / button to play a sepcific Enna voiceline
+              </p>
+              <p class="text-center site-credits">
+                Programming: Alpaka, Art: Matahari
+              </p>
+            </v-card-title>
+          </v-img>
+        </a>
+      </v-card>
+    </div>
+    <div class="site">
+      <v-card height="100%">
+        <a href="https://picrew.me/image_maker/1908383" target="_blank" class="text-decoration-none">
+          <v-img :src="picrewImgUrl" gradient="to bottom, rgba(0,0,0,0), rgba(0,0,0,.7)" class="align-end" cover height="100%">
+            <v-card-title class="text-white pb-8">
+              <h2 class="text-center mb-2">Aloupeeps Maker Game</h2>
+              <p class="text-center site-desc">
+                Aloupeeps dress up to celebrate EnnDay2023!
+              </p>
+              <p class="text-center site-credits">
+                (picrew) Art by <a href="https://twitter.com/quanxb" target="_blank" class="text-white">gummy bear🕊🎐</a><br />
+              </p>
+            </v-card-title>
+          </v-img>
+        </a>
+      </v-card>
+    </div>  
+  </div>
 </template>
 
 <script>
 import ButtonImg from '@/assets/images/bishbtn.png';
+import PicrewImg from '@/assets/images/picrew.png';
 
 export default {
   data() {
     return {
       buttonImgUrl: ButtonImg,
+      picrewImgUrl: PicrewImg,
     }
   },
   mounted() {
@@ -52,7 +54,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.container {
-  width:100%; height:100%; max-width:100%;
+.sites {
+  display:flex;
+  height:100%;
+
+  .site {
+    width:50%;
+    height:100%;
+
+    .site-desc {
+      white-space: normal;
+      line-height:1.3em;
+    }
+  }
 }
 </style>
