693923e8f4f68993cd2b6b56ac0dd46e83e2ad88
dragonjet <jetriconew@gmail.com>
2023-02-20 08:59:35 -0800

    Blessing Teaser

--- src/projects/ProjectBlessing.vue
+++ src/projects/ProjectBlessing.vue
@@ -1,16 +1,86 @@
 <template>
-  <div class="container">
-    <v-img :src="TeasertImg" class="teaser-img" height="100%" />
-  </div>
+  <v-container class="container pa-0 ma-0" style="height:100%;">
+    <v-row no-gutters style="height:100%;">
+      <v-col cols="3" style="height:100%;">
+        <div class="py-2 px-4 pr-0" style="width:100%; height:100%;">
+<pre class="credits">
+Music:
+
+Blessing by halyosy ft. Vocaloid :https://www.youtube.com/watch?v=E4tIH...
+
+MV artist:
+
+Moka
+Tsuki
+JayceeAnimation
+diamonds
+6InchSenpai
+Aeyuak
+
+Special Thanks to the feet drawers:
+
+AinLilth
+Voitan
+night_rias
+Morirgan420
+6InchSenpai
+MAkuma64
+JeiEztli
+SuwucideTM
+KyzoLuxray
+Xiaohou
+Luv_babu
+scriptentropy
+LuvSan6
+Zingler0612
+deragun
+Skai_kun
+TheSubjectO
+Hazel Sokolov
+convallaria0531
+Eyyyybanana
+Elysian Elodie
+aitoirasuto
+
+Recording:
+
+Matahari
+Matahari's grandma
+
+Trailer Organizers:
+
+Matahari
+Bettiepop
+
+Animations:
+
+Stelli_a
+jozceli
+
+Audio:
+
+Heko (mix)
+Koori_Bunny (Kazoo)
+</pre>
+        </div>
+      </v-col>
+      <v-col cols="9">
+        <iframe
+          width="100%" height="100%"
+          src="https://www.youtube-nocookie.com/embed/Yl1kfKnlQvw"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; web-share" 
+          allowfullscreen>
+        </iframe>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import TeasertImg from '@/assets/teaser/blessing.png';
-
 export default {
   data() {
     return {
-      TeasertImg,
     }
   },
   mounted() {
@@ -20,13 +90,13 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  width:100%;
-  height:100%;
-  background:#1a1a1a;
-
-  .teaser-img {
-    max-width:100%;
-    max-height:100%;
+  width:100%; height:100%; max-width:100%;
+  .credits {
+    width:100%;
+    height:100%;
+    white-space: pre !important;
+    font-family:Arial;
+    overflow-y:scroll;
   }
 }
 </style>
