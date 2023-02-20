90a149c81a603ce142f44fb9ed784e3011d463e0
dragonjet <jetriconew@gmail.com>
2023-02-20 09:26:02 -0800

    Cookbook

--- src/projects/ProjectCookbook.vue
+++ src/projects/ProjectCookbook.vue
@@ -1,16 +1,135 @@
 <template>
-  <div class="container">
-    <v-img :src="TeasertImg" class="teaser-img" height="100%" />
-  </div>
+  <v-container class="container pa-0 ma-0">
+    <v-row no-gutters style="height:100%;">
+      <v-col cols="3">
+        <div class="py-2 pr-0 px-4">
+<pre class="credits">
+Graphic designer: 
+Janice (Janice_XwX#5825)
+
+Front page Artwork: 
+A.cat (A.Cat#4415)
+
+Enna Chibis: (2 sprites each x 3 = 6 sprites)
+Rina (novembon (rina)#2449 )
+Phantome (PhanTomE#1413)
+Trisha (Trisha#9710)
+
+Aloupeeps Sprites: (2 sprites each x 2 = 4 sprites)
+Kawa(2KawaQV#7997)
+Kyzo (Kyzo#9850 )
+
+Outfit design
+Moka (Moka#6943)
+Just include the names
+the brackets are discord tags
+</pre>
+        </div>
+      </v-col>
+      <v-col cols="9" style="background:#1a1a1a; height:100%;">
+        <v-carousel height="100%" hide-delimiters progress>
+          <v-carousel-item :src="Cookbook01" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook02" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook03" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook04" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook05" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook06" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook07" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook08" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook09" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook10" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook11" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook12" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook13" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook14" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook15" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook16" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook17" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook18" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook19" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook20" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook21" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook22" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook23" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook24" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook25" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook26" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook27" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook28" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook29" contain ></v-carousel-item>
+          <v-carousel-item :src="Cookbook30" contain ></v-carousel-item>
+        </v-carousel>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import TeasertImg from '@/assets/teaser/cookbook.png';
+import Cookbook01 from '@/assets/cookbook/cb01.png';
+import Cookbook02 from '@/assets/cookbook/cb02.png';
+import Cookbook03 from '@/assets/cookbook/cb03.png';
+import Cookbook04 from '@/assets/cookbook/cb04.png';
+import Cookbook05 from '@/assets/cookbook/cb05.png';
+import Cookbook06 from '@/assets/cookbook/cb06.png';
+import Cookbook07 from '@/assets/cookbook/cb07.png';
+import Cookbook08 from '@/assets/cookbook/cb08.png';
+import Cookbook09 from '@/assets/cookbook/cb09.png';
+import Cookbook10 from '@/assets/cookbook/cb10.png';
+import Cookbook11 from '@/assets/cookbook/cb11.png';
+import Cookbook12 from '@/assets/cookbook/cb12.png';
+import Cookbook13 from '@/assets/cookbook/cb13.png';
+import Cookbook14 from '@/assets/cookbook/cb14.png';
+import Cookbook15 from '@/assets/cookbook/cb15.png';
+import Cookbook16 from '@/assets/cookbook/cb16.png';
+import Cookbook17 from '@/assets/cookbook/cb17.png';
+import Cookbook18 from '@/assets/cookbook/cb18.png';
+import Cookbook19 from '@/assets/cookbook/cb19.png';
+import Cookbook20 from '@/assets/cookbook/cb20.png';
+import Cookbook21 from '@/assets/cookbook/cb21.png';
+import Cookbook22 from '@/assets/cookbook/cb22.png';
+import Cookbook23 from '@/assets/cookbook/cb23.png';
+import Cookbook24 from '@/assets/cookbook/cb24.png';
+import Cookbook25 from '@/assets/cookbook/cb25.png';
+import Cookbook26 from '@/assets/cookbook/cb26.png';
+import Cookbook27 from '@/assets/cookbook/cb27.png';
+import Cookbook28 from '@/assets/cookbook/cb28.png';
+import Cookbook29 from '@/assets/cookbook/cb29.png';
+import Cookbook30 from '@/assets/cookbook/cb30.png';
+
 
 export default {
   data() {
     return {
-      TeasertImg,
+      Cookbook01,
+      Cookbook02,
+      Cookbook03,
+      Cookbook04,
+      Cookbook05,
+      Cookbook06,
+      Cookbook07,
+      Cookbook08,
+      Cookbook09,
+      Cookbook10,
+      Cookbook11,
+      Cookbook12,
+      Cookbook13,
+      Cookbook14,
+      Cookbook15,
+      Cookbook16,
+      Cookbook17,
+      Cookbook18,
+      Cookbook19,
+      Cookbook20,
+      Cookbook21,
+      Cookbook22,
+      Cookbook23,
+      Cookbook24,
+      Cookbook25,
+      Cookbook26,
+      Cookbook27,
+      Cookbook28,
+      Cookbook29,
+      Cookbook30,
     }
   },
   mounted() {
@@ -20,13 +139,13 @@ export default {
 
 <style lang="scss" scoped>
 .container {
-  width:100%;
-  height:100%;
-  background:#1a1a1a;
-
-  .teaser-img {
-    max-width:100%;
-    max-height:100%;
+  width:100%; height:100%; max-width:100%;
+  .credits {
+    width:100%;
+    height:100%;
+    white-space: pre !important;
+    font-family:Arial;
+    overflow-y:scroll;
   }
 }
 </style>
